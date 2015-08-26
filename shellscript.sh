c7dba79ca6196f1429771163b01c907673317f1c
Upendra <upendra@upendra-Inspiron-3537.(none)>
2015-08-26 17:45:44 +0530

    Update the script for dynamic URL

--- shellscript.sh
+++ shellscript.sh
@@ -42,23 +42,65 @@ done
 if [ ! $folder_of_jmeter ];
 then
 folder_of_jmeter=$dir
-fi 
+fi
+if [ ! $url ];
+then
+url="http://cite.opengeospatial.org/te2/"
+fi
+i=0
+testurl=$(echo $url | tr "/" "\n")
+set -- $testurl
+server=$(echo $1 | tr ":" "\n")
+host=$2
+warName=/$3/
 rm index.html
+rm savedata
 directory=`ls -F1 ${folder_of_jmeter} | grep /`
+now=$(date +"%Y/%m/%d  %H:%M:%S")
+echo "<b>TE integration testing - </b>$now<BR/>" >> index.html
+jmeter -n -t $folder_of_jmeter/teamenginePlan.jmx -Juser=$user -Jpassword=$password -Jserver=$server -Jhost=$host -Jwarname=$warName -Jurl=$server://$host/$warName/
+teVersion=$(cat -n $folder_of_jmeter/savedata | grep "&lt;p&gt;" | tail -2)
+teVersion=${teVersion#*&gt;}
+teVersion=${teVersion%&lt;\/p*}
+echo "<BR/><b>TE version :</b> $teVersion" >> index.html
+teBuild=$(cat $folder_of_jmeter/${var}savedata | grep -Po 'lb="teamenginetestResult" rc="\K.*?(?=")')
+teRegister=$(cat $folder_of_jmeter/${var}savedata | grep -Po 'lb="registrationTest" rc="\K.*?(?=")')
+echo "<BR/><b>Default build  :</b> " >> index.html
+if echo $teBuild | grep -q "200";
+then 
+echo "SUCCESS">> index.html
+else
+echo "FAILED">> index.html
+fi
+echo "<BR/><b>User can be created and logins   :</b> " >> index.html
+if echo $teRegister | grep -q "200";
+then 
+echo "SUCCESS<BR/>">> index.html
+else
+echo "FAILED<BR/>">> index.html
+fi
 for var in $directory
 do
 rm $folder_of_jmeter/${var}savedata
-jmeter -n -t $folder_of_jmeter/${var}test.jmx -Juser=$user -Jpassword=$password
-emails=$(cat $folder_of_jmeter/${var}savedata | grep -Po 'lb="checkResult" rc="\K.*?(?=")')
+jmeter -n -t $folder_of_jmeter/${var}test.jmx -Juser=$user -Jpassword=$password -Jserver=$server -Jhost=$host -Jwarname=$warName -Jurl=$server://$host/$warName/
+result=$(cat $folder_of_jmeter/${var}savedata | grep -Po 'lb="checkResult" rc="\K.*?(?=")')
+formResult=$(cat $folder_of_jmeter/${var}savedata | grep -Po 'lb="formResult" rc="\K.*?(?=")')
 testName=$(cat $folder_of_jmeter/${var}savedata | grep -Po 'tn="\K.*?(?=")')
-echo "<b>Test Name :</b> " >> index.html
+echo "<BR/><b>Test Name :</b> " >> index.html
 echo $testName | { read first rest ; echo $first ; } >> index.html
-echo "<BR/><b>Test Run  :</b> " >> index.html
-if echo $emails | grep -q "200";
+echo "<BR/><b>Test can be run  :</b> " >> index.html
+if echo $formResult | grep -q "200";
 then 
-echo "SUCCESS<BR/><BR/>">> index.html
+echo "SUCCESS">> index.html
 else
-echo "FAILED<BR/><BR/>">> index.html
+echo "FAILED">> index.html
+fi
+echo "<BR/><b>Test run finishes successfully  :</b> " >> index.html
+if echo $result | grep -q "200";
+then 
+echo "SUCCESS<BR/>">> index.html
+else
+echo "FAILED<BR/>">> index.html
 fi
 done
 
