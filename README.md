3b79345cb1314ccf63c630f2b3a76ea4b1dd46df
upendra-sachan <upendra.sachan@mindstix.com>
2015-09-29 12:09:01 +0530

    Merged branch 2

--- README.md
+++ README.md
@@ -1,33 +1,16 @@
 # teamengine-integration-testing
+Code and instructions to test teamengine on the web, integrating some ETS and sample files<br/>
 
-## Test suites
-Code and instructions to test teamengine on the web, integrating some ETS and sample files
+* **Specified Key**<br/>
 
-CSW Test.jmx : Contain jmeter script for testing CSW2.0.2 test.<br/>
-GML Test.jmx : Contain jmeter script for testing GML3.2.1 test.<br/>
-KML Test.jmx : Contain jmeter script for testing KML2.2 test.<br/>
-SOS Test.jmx : Contain jmeter script for testing SOS1.0.0 test.<br/>
-WFS1.1.0_Test.jmx : Contain jmeter script for testing WFS1.1.0 test. In this user set **Supported conformance classes** and **GML Simple Features (GMLSF) compliance level** inside test parameter config element.<br/>
-WFS2.0_Test.jmx : Contain jmeter script for testing WFS2.0 test. In this user set **Feature identifier** inside Feature identifier config element.<br/>
-WMS1.3.0_Test.jmx : Contain jmeter script for testing WMS1.3.0 test. In this user set all other parameter inside test parameter config element.<br/>
-filePath.csv : This file contains the xml file path which user wish to test.<br/>
-user.csv : This file user's login credential info.
-
-**Note:** 1. Set the file path of "user.csv" inside "userLoginData" config file.<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2. Set the file path of "filePath.csv" inside "filePathData" config file. 
+ - -f&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:	path of jmeter script<br/>
+ - -u&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:	path of URL<br/>
+ - -user&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:	User name<br/>
+ - -password&nbsp;:	User password<br/>
 
 
-## Target platforms
+* **EXAMPLE**<br/>
+1. sh ./shellscript.sh  -user username -password paswordOfUser -f pathOfAllJmetersFile -u pathOfTeamengineURL
 
-### GNU/Linux
+* file.csv : This file contains the xml file path which user wish to test.<br/>
 
-* __OS__: ?
-* __JDK__: Oracle JDK 7u79?
-* __JEE container__: Apache Tomcat 7.0.?
-
-
-### Windows
-
-* __OS__: ?
-* __JDK__: Oracle JDK 7u79?
-* __JEE container__: Apache Tomcat 7.0.?
---
+++ shellscript.sh
@@ -0,0 +1,130 @@
+#!/bin/sh
+dir=$(pwd)
+
+printHelp(){
+echo "---------"
+echo "-f		:	path of jmeter script"
+echo "-u		:	path of URL"
+echo "-user		:	User name"
+echo "-password	:	User password"
+echo "---------"
+exit 0
+}
+if [ "$1" = "-h" -o "$1" = "--help" ]; then
+printHelp
+fi 
+
+while [ "$1" ]; do
+  key="$1"
+
+  case $key in
+       -f)
+      folder_of_jmeter="$2"
+      shift
+      ;;
+      -u)
+      url="$2"
+      shift
+      ;;
+      -user)
+      user="$2"
+      shift
+      ;;
+      -password)
+      password="$2"
+      shift
+      ;;
+  esac
+  shift
+done 
+
+
+if [ ! $folder_of_jmeter ];
+then
+folder_of_jmeter=$dir
+fi
+if [ ! $url ];
+then
+url="http://cite.opengeospatial.org/te2/"
+fi
+i=0
+testurl=$(echo $url | tr "/" "\n")
+set -- $testurl
+server=$(echo $1 | tr ":" "\n")
+host=$2
+warName=/$3/
+rm index.html
+rm savedata
+rm -rf result
+directory=`ls -F1 ${folder_of_jmeter} | grep /`
+now=$(date +"%Y/%m/%d  %H:%M:%S")
+echo "<b>TE integration testing - </b>$now" >> index1.html
+echo "<BR/><b>URL :</b> $url<BR/>" >> index1.html
+jmeter -n -t $folder_of_jmeter/teamenginePlan.jmx -Juser=$user -Jpassword=$password -Jserver=$server -Jhost=$host -Jwarname=$warName -Jurl=$server://$host/$warName/
+teVersion=$(cat -n $folder_of_jmeter/savedata | grep "&lt;p&gt;" | tail -2)
+teVersion=${teVersion#*&gt;}
+teVersion=${teVersion%&lt;\/p*}
+finalresult="PASS"
+echo "<BR/><b>TE version :</b> $teVersion" >> index1.html
+teBuild=$(cat $folder_of_jmeter/${var}savedata | grep -Po 'lb="teamenginetestResult" rc="\K.*?(?=")')
+teRegister=$(cat $folder_of_jmeter/${var}savedata | grep -Po 'lb="registrationTest" rc="\K.*?(?=")')
+echo "<BR/><b>Default build  :</b> " >> index1.html
+if echo $teBuild | grep -q "200";
+then 
+echo "SUCCESS">> index1.html
+else
+echo "FAILED">> index1.html
+finalresult="FAIL"
+fi
+echo "<BR/><b>User can be created and logins   :</b> " >> index1.html
+if echo $teRegister | grep -q "200";
+then 
+echo "SUCCESS<BR/>">> index1.html
+else
+echo "FAILED<BR/>">> index1.html
+finalresult="FAIL"
+fi
+for var in $directory
+do
+rm $folder_of_jmeter/${var}savedata
+jmeter -n -t $folder_of_jmeter/${var}test.jmx -Juser=$user -Jpassword=$password -Jserver=$server -Jhost=$host -Jwarname=$warName -Jurl=$server://$host/$warName/
+result=$(cat $folder_of_jmeter/${var}savedata | grep -Po 'lb="checkResult" rc="\K.*?(?=")')
+formResult=$(cat $folder_of_jmeter/${var}savedata | grep -Po 'lb="formResult" rc="\K.*?(?=")')
+testName=$(cat $folder_of_jmeter/${var}savedata | grep -Po 'tn="\K.*?(?=")')
+echo "<BR/><b>Test Name :</b> " >> index1.html
+echo $testName | { read first rest ; echo $first ; } | cut -c1-3 >> index1.html
+string=$(xmllint --xpath "//testResults/httpSample[@lb='formResult']/java.net.URL" $folder_of_jmeter/${var}savedata )
+var=$(echo $string | awk -F"&amp;" '{print $1,$2,$3}')
+set -- $var
+var1=$(echo $3 | awk -F"_" '{print $1,$2,$3,$4}')
+set -- $var1
+echo " $3 revision $4" >> index1.html
+echo "<BR/><b>Test can be run  :</b> " >> index1.html
+if echo $formResult | grep -q "200";
+then 
+echo "SUCCESS">> index1.html
+else
+echo "FAILED">> index1.html
+finalresult="FAIL"
+fi
+echo "<BR/><b>Test run finishes successfully  :</b> " >> index1.html
+if echo $result | grep -q "200";
+then 
+echo "SUCCESS<BR/>">> index1.html
+else
+echo "FAILED<BR/>">> index1.html
+finalresult="FAIL"
+fi
+done
+mkdir result
+if echo $finalresult | grep -q "PASS";
+then
+echo "<b>Final Result :</b> SUCCESS<BR/><BR/>" >> index.html
+echo -n "" > result/SUCCESS
+else
+echo "<b>Final Result :</b> FAILED<BR/><BR/>" >> index.html
+echo -n "" > result/FAIL
+fi
+index1=$(cat index1.html)
+echo $index1 >> index.html
+rm index1.html
